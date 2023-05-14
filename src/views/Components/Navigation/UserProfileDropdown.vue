a7efac45eac8e1e8d9636445c5737246f69ca09c
rafl00 <rafalpopiolek0024@gmail.com>
2023-05-14 22:11:03 +0200

    Remove unnecessary items from user profile dropdown

--- src/views/Components/Navigation/UserProfileDropdown.vue
+++ src/views/Components/Navigation/UserProfileDropdown.vue
@@ -23,19 +23,7 @@
             <li>
                 <a href="#"
                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Dashboard
-                </a>
-            </li>
-            <li>
-                <a href="#"
-                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                     Settings
-                </a>
-            </li>
-            <li>
-                <a href="#"
-                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Earnings
                 </a>
             </li>
             <li>
